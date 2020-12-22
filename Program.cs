454beb79b3ecf0a24af94b2cc367bf22ec887eaa
Brian Muturi <muturibrian18@gmail.com>
2020-12-22 01:33:35 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a:");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Enter b:");
+            double b = double.Parse(Console.ReadLine());
+            double c = a * b;
+            double area = c;
+            Console.Write("The result is:" + c);
+        }
+    }
+}
